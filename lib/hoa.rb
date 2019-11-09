3e091ce9780123c9c8ac23295746356e0410fbd4
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-09 21:54:09 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,7 +6,9 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  show[name]
+  show = :third_earthers
+  name = "Snarf"
+  
   # Write your implementation here
   # Should return the array of the 'show' argument
 end
